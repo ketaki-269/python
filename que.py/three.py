86ee0686620389759098a20517a323ac0c519664
Ketaki Makode <ketakimakode@gmail.com>
2025-09-23 19:04:31 +0530

    Update file

---
+++ que.py/three.py
@@ -0,0 +1,20 @@
+# print table of 2 
+# num=int(input("Enter a number : "))
+# i=int
+# ans=num*i
+# for i in range (1,11):
+#     print (ans)
+
+# print table of 1-10
+
+for i in range (1,11):
+    for j in range(1,11):
+        print(i*j,end=" ")
+    print()    
+
+# target sum ?
+# ex={1,2,6,7}    target =7
+# ouput=1,6
+
+# ex{3,6,1,5,4} target = 10
+# output = 6,4
