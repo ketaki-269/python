55c065c28c6ac0132cea9903311cc44a59a509b9
Ketaki Makode <ketakimakode@gmail.com>
2025-09-25 10:29:47 +0530

    Update file

--- que.py/three.py
+++ que.py/three.py
@@ -31,16 +31,16 @@
 # ex{3,6,1,5,4} target = 10
 # output =6,4 
 
-list=[3,6,1,5,4]
-target=10
-ans=False
-for i in range (len(list)):
-    for j in range (i+1,len(list)):
-        if list[i]+ list[j]==target:
-            print (list[i],list[j])
-            ans = True
-    if ans==True:
-        break
+# list=[3,6,1,5,4]
+# target=10
+# ans=False
+# for i in range (len(list)):
+#     for j in range (i+1,len(list)):
+#         if list[i]+ list[j]==target:
+#             print (list[i],list[j])
+#             ans = True
+#     if ans==True:
+#         break
         
 # print prinme number ?        
 n = int(input("Enter a number: "))
