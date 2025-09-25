e4a6a9361394f4dd2cc917e30ac0223b9f8e2411
Ketaki Makode <ketakimakode@gmail.com>
2025-09-25 10:29:30 +0530

    Update file

--- que.py/three.py
+++ que.py/three.py
@@ -43,3 +43,15 @@ for i in range (len(list)):
         break
         
 # print prinme number ?        
+n = int(input("Enter a number: "))
+
+print("Prime numbers up to", n, "are:")
+
+for num in range(2, n + 1):
+    is_prime = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=" ")
