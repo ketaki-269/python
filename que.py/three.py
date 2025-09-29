8b2670ecdc070c16ed2648cb6ea982ba7069e96d
Ketaki Makode <ketakimakode@gmail.com>
2025-09-29 18:42:43 +0530

    Update file

--- que.py/three.py
+++ que.py/three.py
@@ -123,17 +123,21 @@
 # print(result)  
 
 
-nums = [3, 0, 5, 6, 0, 7]
+# nums = [3, 0, 5, 6, 0, 7]
 
-pos = 0
-for num in nums:
-    if num != 0:
-        nums[pos] = num
-        pos += 1
+# pos = 0
+# for num in nums:
+#     if num != 0:
+#         nums[pos] = num
+#         pos += 1
 
-while pos < len(nums):
-    nums[pos] = 0
-    pos += 1
+# while pos < len(nums):
+#     nums[pos] = 0
+#     pos += 1
 
-print(nums) 
+# print(nums) 
+
+
+# q-1 armstrong number ?
+
 
