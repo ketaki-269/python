7c61cdd85fc6dd30b52edef637a402727d34a1ec
Ketaki Makode <ketakimakode@gmail.com>
2025-10-03 18:39:37 +0530

    Update file

--- que.py/three.py
+++ que.py/three.py
@@ -259,12 +259,14 @@
 #     b =c
 #     print(b, end=" ")
 
-num =int(input("Enter the number: "))
-a=0
-b=1
-print(a,b, end=" ")
-for i in range(num-2) :
-    c= a+b
-    a= b
-    b=c
-    print(b, end=" ")
+# num =int(input("Enter the number: "))
+# a=0
+# b=1
+# print(a,b, end=" ")
+# for i in range(num-2) :
+#     c= a+b
+#     a= b
+#     b=c
+#     print(b, end=" ")
+
+
