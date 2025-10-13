af866cfd78f0a9bbc1d9c9bbc0307f1df85485cf
Ketaki Makode <ketakimakode@gmail.com>
2025-10-13 19:07:04 +0530

    Update file

--- function/lambda.py
+++ function/lambda.py
@@ -14,13 +14,36 @@
 # print(plusone(6))
 
 #max number 
-ans=lambda x:x+1
-print(ans(60))
+# ans=lambda x:x+1
+# print(ans(60))
 
-def maxnum(a,b):
-    if a>b:
-        return a
-    else:
-        return b
-ans=lambda x,y:x if x>y else y
-print(ans(8,70))     
+# def maxnum(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# ans=lambda x,y:x if x>y else y
+# print(ans(8,70))     
+
+
+# que 
+# 1 Write a lambda function to add two numbers.
+# Input: a=5, b=7 → Output: 12
+# ans =  lambda a,b:a+b
+# print(ans(5,7))
+# 2 Write a lambda function to find th+e square of a number.
+# Input: 5 → Output: 25
+# ans=lambda num:  num*num
+# print(ans(5))
+# 3 Write a lambda function to find the maximum of two numbers.
+# ans=lambda x,y:x if x>y else y
+# print(ans(8,70))  
+
+# 4 Write a lambda function to check if a number is even or odd.
+# Input: 7 → Output: "Odd"
+ans=lambda num:"even" if  num%2==0 else "odd"
+print(ans(7))
+# 5 Write a lambda function to get the length of a given string.
+# Input: "Mahesh" → Output: 6
+x="mahesh"
+ans =lambda x :len(x)
