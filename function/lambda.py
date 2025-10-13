2f67226007dd8faaa437a1e8329881d8ca646766
Ketaki Makode <ketakimakode@gmail.com>
2025-10-13 18:48:12 +0530

    Update file

---
+++ function/lambda.py
@@ -0,0 +1,6 @@
+# lambda fucntion is also known as anonymsa function  or single lin function 
+# is it use with map filter and 
+# syntax- lambda argument :code/logic
+def add (a,b):
+    return a+b
+print(add(5,5))
