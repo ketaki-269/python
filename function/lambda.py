2636c4be9b047cada54caa70e69a742279de455a
Ketaki Makode <ketakimakode@gmail.com>
2025-10-13 18:55:25 +0530

    Update file

--- function/lambda.py
+++ function/lambda.py
@@ -1,6 +1,26 @@
 # lambda fucntion is also known as anonymsa function  or single lin function 
 # is it use with map filter and 
 # syntax- lambda argument :code/logic
-def add (a,b):
-    return a+b
-print(add(5,5))
+# def add (a,b):
+#     return a+b
+# print(add(5,5))
+# # convert this in lambda
+# ans =  lambda a,b:a+b
+# print(ans(5,6))
+
+# in single argument
+# def plusone(num):
+#     return num+1
+# print(plusone(6))
+
+#max number 
+ans=lambda x:x+1
+print(ans(60))
+
+def maxnum(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+ans=lambda x,y:x if x>y else y
+print(ans(8,70))     
