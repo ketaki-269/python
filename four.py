1ed0b8c7bd63730c5443355991da3510049c27c6
Ketaki Makode <ketakimakode@gmail.com>
2025-10-25 18:51:38 +0530

    Update file

--- four.py
+++ four.py
@@ -1,15 +1,21 @@
 # comparison or relational operator
 
-a=10
-b=20
-print (a>b)
-print (a<b)
-print (a>=b)
-print (a<=b)
-print (a==b)
-print (a!=b)
+# a=10
+# b=20
+# print (a>b)
+# print (a<b)
+# print (a>=b)
+# print (a<=b)
+# print (a==b)
+# print (a!=b)
 
-x = 5
-y = 5.0
-print(x == y)
-print(x is y)
+# x = 5
+# y = 5.0
+# print(x == y)
+# print(x is y)
+x = 10
+print(5 < x < 15)
+
+a = 90
+b = 90
+print(a != b)
