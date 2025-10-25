4256976b675fe8890031be164ae5386f3667e6b5
Ketaki Makode <ketakimakode@gmail.com>
2025-10-25 18:56:58 +0530

    Update file

--- five.py
+++ five.py
@@ -2,16 +2,19 @@
 
 # and or  not
 # not means reverse or opposite
-print (0 and 10)
-print (0 or 10)
+# print (0 and 10)
+# print (0 or 10)
 
 
-print (not 10)
-print(not 0)
+# print (not 10)
+# print(not 0)
  
-a=20
-b=40
-c=90
-print(a>b and a>c)
-print(b>a and b>c)
-print(c>a and c>b )
+# a=20
+# b=40
+# c=90
+# print(a>b and a>c)
+# print(b>a and b>c)
+# print(c>a and c>b )
+
+x = 5
+print(x < 0 or x == 5)
