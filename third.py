83a44fb2be9a4dde309aea84e9bd8631157d499e
Ketaki Makode <ketakimakode@gmail.com>
2025-10-25 16:14:42 +0530

    Update file

--- third.py
+++ third.py
@@ -1,12 +1,17 @@
 # assignments operators 
-a=90
-b=10
-# a=a+b
-# a+=b
-# a-=b
-a*=b
-a/=b
-a//=b
-a%=b
-print(a)
+# a=90
+# b=10
+# # a=a+b
+# # a+=b
+# # a-=b
+# a*=b
+# a/=b
+# a//=b
+# a%=b
+# print(a)
 
+# x = 10
+# print(x)
+x = 5
+x -= 3
+print(x)
