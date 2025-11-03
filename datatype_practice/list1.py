4a64a57217521bab1735d69e1bdd3dd6b92fd08e
Ketaki Makode <ketakimakode@gmail.com>
2025-11-03 14:00:13 +0530

    Update file

--- datatype_practice/list1.py
+++ datatype_practice/list1.py
@@ -13,6 +13,16 @@
 
 #accessing elements of list
 # this can be done in to ways by using indexing or slicing 
+#  with indexing
+# list=[10,"A","B",20,30] # like we we want to access "A" from this we can easily access this with its index number 
+# print(list[1])
+# print(list[-1])
 
-list=[10,"A","B",20,30] # like we we want to access "A" from this we can easily access this with its index number 
-print(list[1])
+
+#with slicing
+n=[1,2,3,4,5,6,7,8,9,10]
+print(n[2:7:2])
+print(n[4::2])
+print(n[3:7])
+print(n[8:2:-2])
+print(n[4:100])
