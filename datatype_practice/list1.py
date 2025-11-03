017bc63ab53fd3cc92ef1420bbcbd383057a77f2
Ketaki Makode <ketakimakode@gmail.com>
2025-11-03 13:43:23 +0530

    Update file

---
+++ datatype_practice/list1.py
@@ -0,0 +1,18 @@
+#creation
+# list=[10,"A","B",20,30]
+# print(list)
+# print(type(list))
+
+#creation with dynamic input
+# list=[]
+# n=int(input("Enter number of elements going to be in List"))
+# for i in range(0,n):   
+#  i=input()
+#  list.append(i)
+# print(list)
+
+#accessing elements of list
+# this can be done in to ways by using indexing or slicing 
+
+list=[10,"A","B",20,30] # like we we want to access "A" from this we can easily access this with its index number 
+print(list[1])
