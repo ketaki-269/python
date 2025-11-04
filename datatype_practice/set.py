85541e892726ad0cd2630bf4498a7a3426d8b5ef
Ketaki Makode <ketakimakode@gmail.com>
2025-11-04 13:30:11 +0530

    Update file

--- datatype_practice/set.py
+++ datatype_practice/set.py
@@ -11,11 +11,11 @@
 # while creating empty set we should use set () function as if not complier will treat it as dictionary 
 
 # like this wikl create dict not set
-s={}
-print(s)
-print(type(s))
+# s={}
+# print(s)
+# print(type(s))
 
 # this will create set
-s=set()
-print(s)
-print(type(s))
+# s=set()
+# print(s)
+# print(type(s))
