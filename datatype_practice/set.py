adc12ea13a3f9f400ae647fa8d1dedf66ca89da3
Ketaki Makode <ketakimakode@gmail.com>
2025-11-04 13:14:57 +0530

    Update file

--- datatype_practice/set.py
+++ datatype_practice/set.py
@@ -1,3 +1,15 @@
-s={10,20,30,40}
+#createion of set
+# s={10,20,30,40}
+# print(s)
+# print(type(s))
+
+#
+# s=set(range(5))
+# print(s)
+
+
+# while creating empty set we should use set () function as if not complier will treat it as dictionary 
+
+s={}
 print(s)
 print(type(s))
