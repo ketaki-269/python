6c7b02b973f90d4e092b78296f0fe4a97ced94de
Ketaki Makode <ketakimakode@gmail.com>
2025-11-05 14:30:27 +0530

    Update file

--- datatype_practice/dict.py
+++ datatype_practice/dict.py
@@ -1,5 +1,21 @@
-d={}
-d[100]="durga"
-d[200]="ravi"
-d[300]="shiva"
+#createion 
+# d={}
+# d[100]="durga"
+# d[200]="ravi"
+# d[300]="shiva"
+# print(d)
+
+# d={100: 'durga', 200: 'ravi', 300: 'shiva'}
+# print(d[300])
+
+# to update 
+# d={100:"durga",200:"ravi",300:"shiva"}
+# print(d)
+# d[400]="pavan"
+# print(d)
+
+# to ddelet
+d={100:"durga",200:"ravi",300:"shiva"}
+print(d)
+del d[100]
 print(d)
