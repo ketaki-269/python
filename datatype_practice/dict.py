ae8f64c79debed1a555c9407493c69094848fe85
Ketaki Makode <ketakimakode@gmail.com>
2025-11-05 15:15:01 +0530

    Update file

--- datatype_practice/dict.py
+++ datatype_practice/dict.py
@@ -26,6 +26,55 @@
 # print(d)
 
 # to delete dict 
+# d={100:"durga",200:"ravi",300:"shiva"}
+# del d
+# print(d)
+
+# fucntions
+# 1.dict 
+# d={100:"durga",200:"ravi",300:"shiva"}
+# dict(d)
+# 2.len()
+# d={100:"durga",200:"ravi",300:"shiva"}
+# print(len(d))
+# 3.clear() 
+# d={100:"durga",200:"ravi",300:"shiva"}
+# d.clear()
+# print(d)
+# print(clear(d))
+# 4.get()
+# d={100:"durga",200:"ravi",300:"shiva"}
+# print(d.get(100))
+# 5.pop()
+# d={100:"durga",200:"ravi",300:"shiva"}
+# print(d.pop(100))
+# print(d)
+# 6.pop_item()
+# d={100:"durga",200:"ravi",300:"shiva"}
+# print(d.popitem())
+# print(d)
+# 7.keys()
+# d={100:"durga",200:"ravi",300:"shiva"}
+# print(d.keys())
+# print(d)
+# 8.values()
+# d={100:"durga",200:"ravi",300:"shiva"}
+# print(d.values())
+# print(d)
+# 9.items()
+# d={100:"durga",200:"ravi",300:"shiva"}
+# print(d.items())
+# print(d)
+# 10.copy()
 d={100:"durga",200:"ravi",300:"shiva"}
-del d
+d1=d.copy()
+print(d1)
+# 11.setdefualt()
+d={100:"durga",200:"ravi",300:"shiva"}
+print(d.setdefault(10,"a"))
+print(d)
+# 12.update()
+d={100:"durga",200:"ravi",300:"shiva"}
+x={20:"A",10:"B"}
+print(d.update(x))
 print(d)
--- datatype_practice/tempCodeRunnerFile.py
+++ datatype_practice/tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-update
+d={100:"durga",200:"ravi",300:"shiva"}
+print(d.items())
+print(d)
