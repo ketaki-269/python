30932d5a6603cb5905bdf50162f8cf6c2c51f11c
Ketaki Makode <ketakimakode@gmail.com>
2025-11-19 17:44:24 +0530

    Update file

---
+++ practicenov/com_one.py
@@ -0,0 +1,54 @@
+# 1. List Comprehension Questions
+
+# 1. Generate a list of even numbers from 1 to 20.
+# 2. Convert all characters of a string "python" into uppercase.
+# 3. Create a list of cubes of odd numbers from 1 to 15.
+# 4. Extract vowels from the string "comprehension".
+# 5. From a list [10, 25, 30, 45, 60, 75, 90], create a new list containing only numbers
+# greater than 40.
+# 6. Create a list of the first letter of each word in ["apple", "banana", "cherry"].
+# 7. Flatten a nested list [[1,2],[3,4],[5,6]] into a single list.
+# 8. Create a list of (x, y) pairs where both are between 1 and 3 (Cartesian product).
+# 9. Extract all words longer than 4 letters from "Python list comprehension is powerful".
+# 10. From a sentence, extract words that start with a vowel.
+
+# 2. Tuple Comprehension (using tuple())
+
+# 1. Create a tuple of squares from 1 to 5.
+# tuple(x**2 for x in range(1,6))
+# 2. Create a tuple of even numbers from 2 to 10.
+# 3. Convert a list ['a', 'b', 'c'] to uppercase letters as a tuple.
+# 4. Create a tuple of cubes of numbers from 1 to 7.
+# 5. Create a tuple of lengths of words in ["hi", "python", "list"].
+# 6. Generate a tuple of numbers divisible by 7 between 1 and 50.
+# 7. Convert the characters of a string "hello" into a tuple.
+# 8. Create a tuple of all vowels present in "education".
+
+# 3. Set Comprehension Questions
+
+# 1. Create a set of squares from 1 to 10.
+# {x**2 for x in range(1,11)}
+# 2. Create a set of even numbers from 1 to 20.
+# 3. Create a set of characters from "banana".
+# 4. From [1,2,2,3,4,4,5], create a set of unique elements.
+# 5. Create a set of numbers from 1–30 divisible by 3 or 5.
+# 6. Generate a set of cubes for numbers 1–10 but only include cubes < 100.
+# 7. Create a set of squares for only odd numbers between 1 and 15.
+
+# 4. Dictionary Comprehension Questions
+
+# 1. Create a dictionary where keys are numbers from 1 to 5 and values are their squares.
+# {x: x**2 for x in range(1,6)}
+# 2. Create a dictionary from two lists:
+# keys = ['name', 'age', 'city']
+# values = [‘Jatin’, 22, 'Bhopal']
+# {‘name’: ‘Jatin’, ‘age’:22, ‘city’: ‘Bhopal’}
+# 3. Convert all keys of a dictionary to uppercase.
+# 4. Count the frequency of each character in "python".
+# 6. Swap keys and values of a dictionary.
+# 7. Filter dictionary items where value > 10.
+# 8. Create a dictionary of vowels and their ASCII values.
+# 11. Create a dictionary mapping each word in a sentence to its length.
+# keys = ['a','b','c','d','e']
+# values = [1,2,3,4,5]
+# 12. Given a dictionary of student marks, extract students who scored above 80.
