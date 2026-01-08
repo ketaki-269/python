37e5c84d27a2b35a7dee15993477f0ad1ff0979f
Ketaki Makode <ketakimakode@gmail.com>
2026-01-08 22:15:00 +0530

    Update file

--- nine.py
+++ nine.py
@@ -65,15 +65,15 @@ print(ans)
 # print (ans)
 
 # #13. check the number is multiple of 3 or not 
-# num=int(input("Enter a number: "))
-# result="Multiple" if num%3==0 else"Not Multiple"
-# print(result)
+num=int(input("Enter a number: "))
+result="Multiple" if num%3==0 else"Not Multiple"
+print(result)
 
 # #14. check username=user@gmail.com password=1234 
-# username=input("Enter username: ")
-# password=int(input("Enter Password:"))
-# ans = "Successful " if password==1234 and username =="user@gmail.com"  else "NOt"
-# print (ans)
+username=input("Enter username: ")
+password=int(input("Enter Password:"))
+ans = "Successful " if password==1234 and username =="user@gmail.com"  else "NOt"
+print (ans)
 
 # #15. check number is divisible by 2 and 4
 # num=int(input("Enter number :"))
