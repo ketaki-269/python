b40b452463818d48d78a5af1588a05e5404387f7
Ketaki Makode <ketakimakode@gmail.com>
2026-02-14 23:31:32 +0530

    updated feature

--- function/highorder2.py
+++ function/highorder2.py
@@ -20,8 +20,8 @@
 # print (ans)
 #4  Filter strings with length greater than 4.
 # Input: ["apple", "bat", "ball", "hi"] → Output: ["apple", "ball"]
-lit=["apple", "bat", "ball", "hi"]
-ans=list(filter(lambda x: len(x)>4,lit))
+lisst=["apple", "bat", "ball", "hi"]
+ans=list(filter(lambda x: len(x)>4,lisst))
 print (ans)
 #5  Filter positive numbers.
 # Input: [-2, 0, 5, 9, -7, 3] → Output: [5, 9, 3]
