3c5e53efe2c91219f45a26c9cafe2916723b1ab5
Ketaki Makode <ketakimakode@gmail.com>
2026-02-23 22:31:04 +0530

    updated feature

--- function/intro.py
+++ function/intro.py
@@ -34,9 +34,9 @@
 #     return num+1
 # print(plusone(2))
 
-def hii():
-    return"hii"
-print(hii())
+def hello():
+    return"hello"
+print((hello))
 
 # (this is return type and with argument ) 
 # def addtion(a,b):
