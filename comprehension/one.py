fb138298e45be7cf72d4552368dfdaec88c1ae2a
Ketaki Makode <ketakimakode@gmail.com>
2026-03-14 23:17:05 +0530

    updated feature

--- comprehension/one.py
+++ comprehension/one.py
@@ -9,8 +9,8 @@
 # print(li)
 
 # WAP to store data of table
-dat=[i for i in range (2,21,2)]
-print (dat)
+data=[i for i in range (2,21,2)]
+print (data)
 
 # WAP evenn num.
 # data=[i for i in range (1,21,) if i%2==0]
